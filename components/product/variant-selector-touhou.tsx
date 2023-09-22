190d8742ea23bd2d834fd516441545468a996b97
Oleksandr F <boltbright525@gmail.com>
2023-09-22 21:56:47 +0400

    display states of a variant

--- components/product/variant-selector-touhou.tsx
+++ components/product/variant-selector-touhou.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import clsx from 'clsx';
 import { ProductOption, ProductVariant } from 'lib/shopify/types';
 import { createUrl } from 'lib/utils';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
@@ -41,7 +42,7 @@ export function VariantSelector({
     (
       accumulator: {
         [optTitle: string]: {
-          variants: { id: string; title: string }[];
+          variants: { id: string; title: string; availableForSale: boolean }[];
         };
       },
       variant
@@ -49,15 +50,22 @@ export function VariantSelector({
       const optAndVarTitleRegex = /(\w+)_(.+)/;
       const optAndVarTitle = optAndVarTitleRegex.exec(variant.title);
       if (optAndVarTitle === null) return accumulator;
+
       const [, optTitle, varTitle] = optAndVarTitle;
       if (!optTitle || !varTitle) return accumulator;
+
+      const variantObj = {
+        id: variant.id,
+        title: varTitle,
+        availableForSale: variant.availableForSale
+      };
 
       return {
         ...accumulator,
         [optTitle]: {
           variants: accumulator[optTitle]?.variants
-            ? [...accumulator[optTitle]!.variants, { id: variant.id, title: varTitle }]
-            : [{ id: variant.id, title: varTitle }]
+            ? [...accumulator[optTitle]!.variants, variantObj]
+            : [variantObj]
         }
       };
     },
@@ -74,19 +82,32 @@ export function VariantSelector({
       <dd className="flex flex-wrap gap-3">
         {variants.variants.map((variant) => {
           const optNameLowerCase = options[0]!.name.toLowerCase();
+          const variantName = `${optTitle} ${variant.title}`;
           const optSearchParams = new URLSearchParams(searchParams.toString());
-          optSearchParams.set(optNameLowerCase, `${optTitle} ${variant.title}`);
+          optSearchParams.set(optNameLowerCase, variantName);
           const optUrl = createUrl(pathname, optSearchParams);
+
+          const isActive = searchParams.get(optNameLowerCase) === variantName;
 
           return (
             <button
               key={variant.id}
+              aria-disabled={!variant.availableForSale}
+              disabled={!variant.availableForSale}
+              title={!variant.availableForSale ? 'Out of Stock' : ''}
               onClick={() => {
                 router.replace(optUrl, { scroll: false });
               }}
-              className={
-                'flex min-w-[48px] items-center justify-center rounded-full border bg-neutral-100 px-2 py-1 text-sm dark:border-neutral-800 dark:bg-neutral-900'
-              }
+              className={clsx(
+                'flex min-w-[48px] items-center justify-center rounded-full border bg-neutral-100 px-2 py-1 text-sm dark:border-neutral-800 dark:bg-neutral-900',
+                {
+                  'cursor-default ring-2 ring-blue-600': isActive,
+                  'ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-blue-600 ':
+                    !isActive && variant.availableForSale,
+                  'relative z-10 cursor-not-allowed overflow-hidden bg-neutral-100 text-neutral-500 ring-1 ring-neutral-300 before:absolute before:inset-x-0 before:-z-10 before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform dark:bg-neutral-900 dark:text-neutral-400 dark:ring-neutral-700 before:dark:bg-neutral-700':
+                    !variant.availableForSale
+                }
+              )}
             >
               {variant.title}
             </button>
