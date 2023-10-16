d35096c797f19171885912c9d561619a43cfee2f
Oleksandr F <boltbright525@gmail.com>
2023-10-16 13:55:40 +0400

    make ProductGridItems accept classes

--- components/layout/product-grid-items.tsx
+++ components/layout/product-grid-items.tsx
@@ -1,13 +1,20 @@
+import clsx from 'clsx';
 import Grid from 'components/grid';
 import { GridTileImage } from 'components/grid/tile';
 import { Product } from 'lib/shopify/types';
 import Link from 'next/link';
 
-export default function ProductGridItems({ products }: { products: Product[] }) {
+export default function ProductGridItems({
+  products,
+  className
+}: {
+  products: Product[];
+  className?: string;
+}) {
   return (
     <>
       {products.map((product) => (
-        <Grid.Item key={product.handle} className="animate-fadeIn">
+        <Grid.Item key={product.handle} className={clsx('animate-fadeIn', className)}>
           <Link className="relative inline-block h-full w-full" href={`/product/${product.handle}`}>
             <GridTileImage
               alt={product.title}
