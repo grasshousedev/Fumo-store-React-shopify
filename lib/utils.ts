4881b8f6ea5ca5e805a8139fadfaf899edee1d9e
Oleksandr F <boltbright525@gmail.com>
2023-10-16 22:17:13 +0400

    install shadcn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,10 @@
+import { clsx, type ClassValue } from 'clsx';
 import { ReadonlyURLSearchParams } from 'next/navigation';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 export const createUrl = (pathname: string, params: URLSearchParams | ReadonlyURLSearchParams) => {
   const paramsString = params.toString();
