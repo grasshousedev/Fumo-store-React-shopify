93a4337db0344642bacf27064ed075754353c74a
Oleksandr F <boltbright525@gmail.com>
2023-10-17 14:45:24 +0400

    add CTA button to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Grid from 'components/grid';
-import Footer from 'components/layout/footer';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
-import CollectionGridItems from 'components/layout/collection-grid-items';
 import { getCollections } from 'lib/shopify';
 
+import Grid from 'components/grid';
+import CollectionGridItems from 'components/layout/collection-grid-items';
+import Footer from 'components/layout/footer';
 import Logo from 'components/logo';
+
+import { buttonVariants } from '@/components/ui/button';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
 
 export const runtime = 'edge';
 
@@ -22,11 +26,15 @@ export default async function HomePage() {
   return (
     <>
       <Suspense>
-        <div className="mx-auto max-w-screen-xl px-4 py-4">
+        <div className="mx-auto max-w-screen-xl px-4 py-4 text-center">
           <Logo className="mx-auto mb-8 w-2/3 max-w-md" /> {/* //TODO change mb */}
-          <Grid className="grid-cols-1">
+          <Grid className="mb-10 grid-cols-1">
             <CollectionGridItems collections={collections} />
           </Grid>
+          <Link href="/search" className={buttonVariants({ size: 'lg', className: 'gap-1' })}>
+            <span className="text-base">See all our collections</span>
+            <ChevronRightIcon className="h-5 w-5" />
+          </Link>
         </div>
         <Footer />
       </Suspense>
