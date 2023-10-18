fd602588b05d4d7e90bae016ce6e96f7318f25b0
Oleksandr F <boltbright525@gmail.com>
2023-10-18 17:17:10 +0400

    use flex instead of grid for collections on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import Link from 'next/link';
 import { Suspense } from 'react';
 
-import { getCollections } from 'lib/shopify';
-
-import Grid from 'components/grid';
-import CollectionGridItems from 'components/layout/collection-grid-items';
 import Footer from 'components/layout/footer';
 import Logo from 'components/logo';
 
 import { buttonVariants } from '@/components/ui/button';
 import { ChevronRightIcon } from '@heroicons/react/24/outline';
+
+import CollectionsList from '@/components/layout/collections-list';
 
 export const runtime = 'edge';
 
@@ -21,16 +19,12 @@ export const metadata = {
 };
 
 export default async function HomePage() {
-  const collections = await getCollections();
-
   return (
     <>
       <Suspense>
         <div className="mx-auto max-w-screen-xl px-4 py-4 text-center">
           <Logo className="mx-auto mb-8 w-2/3 max-w-md" /> {/* //TODO change mb */}
-          <Grid className="mb-10 grid-cols-1">
-            <CollectionGridItems collections={collections} />
-          </Grid>
+          <CollectionsList />
           <Link href="/search" className={buttonVariants({ size: 'lg', className: 'gap-1' })}>
             <span className="text-base">See all our collections</span>
             <ChevronRightIcon className="h-5 w-5" />
---
+++ components/layout/collections-list.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { GridTileImage } from '@/components/grid/tile';
+
+import { getCollections } from 'lib/shopify';
+
+export default async function CollectionsList() {
+  const collections = await getCollections();
+
+  return (
+    <ul className="mb-10 flex animate-fadeIn overflow-hidden rounded-lg border border-neutral-200 pb-4">
+      {collections.map(
+        (collection) =>
+          //* dont't render the collection if it doesn't have an image
+          !!collection.image && (
+            <li key={collection.handle} className="w-full">
+              <Link className="mb-[4%] inline-block w-full" href={collection.path}>
+                <Image
+                  className="min-h-[55px] w-full object-cover" //* final min-h TBD
+                  alt={collection.image.altText ?? ''}
+                  src={collection.image.url}
+                  width={collection.image.width}
+                  height={collection.image.height}
+                  sizes="70vw"
+                />
+              </Link>
+              <ul className="mx-[2%] flex gap-4">
+                {collection.products.map((product) => (
+                  <li
+                    key={product.handle}
+                    className="hidden aspect-square w-full animate-fadeIn first:block md:last:block sm:[&:nth-child(2)]:block"
+                  >
+                    <Link
+                      className="relative inline-block h-full w-full"
+                      href={`/product/${product.handle}`}
+                    >
+                      <GridTileImage
+                        alt={product.title}
+                        label={{
+                          title: product.title,
+                          amount: product.variants[0]!.price.amount,
+                          currencyCode: product.variants[0]!.price.currencyCode
+                        }}
+                        src={product.featuredImage?.url}
+                        fill
+                        sizes="(min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
+                      />
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          )
+      )}
+    </ul>
+  );
+}
