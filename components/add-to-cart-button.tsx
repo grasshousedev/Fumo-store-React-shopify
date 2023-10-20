e941a9eac7302b217bad3e295d7b69cce0f6adbe
Oleksandr F <boltbright525@gmail.com>
2023-10-20 20:59:01 +0400

    use ghost button variant for add to cart button if the variant isn't available

--- components/add-to-cart-button.tsx
+++ components/add-to-cart-button.tsx
@@ -43,7 +43,7 @@ export default function AddToCartButton({
 
         router.refresh();
       }}
-      variant="glassmorphism"
+      variant={productVariant.availableForSale ? 'glassmorphism' : 'ghost'}
       size="icon"
     >
       {!isPending ? (
