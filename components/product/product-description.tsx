b4baca6e79a3f2322a6f57b12418ee523aab8fc8
Oleksandr F <boltbright525@gmail.com>
2023-10-26 20:44:05 +0400

    replace <Gallery> with a keen-slider

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -1,10 +1,15 @@
+'use client';
+
 import { AddToCart } from '@/components/product/add-to-cart';
-import { Gallery } from '@/components/product/gallery';
+// import { Gallery } from '@/components/product/gallery';
 import Price from '@/components/ui/price';
 import Prose from '@/components/ui/prose';
 import { Money, Product } from 'lib/shopify/types';
 import { VariantSelector } from './variant-selector';
 import { VariantSelectorWithPseudoOptions } from './variant-selector-pseudo-options';
+
+import { ThumbnailPlugin } from '@/lib/keen-slider';
+import { useKeenSlider } from 'keen-slider/react';
 
 export function ProductDescription({
   product,
@@ -13,21 +18,49 @@ export function ProductDescription({
   product: Product;
   selectedVariantPrice: Money;
 }) {
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+    created() {
+      console.log('slider created');
+    }
+  });
+
+  const [thumbnailRef] = useKeenSlider<HTMLDivElement>(
+    {
+      initial: 0,
+      slides: {
+        perView: 4,
+        spacing: 10
+      }
+    },
+    [ThumbnailPlugin(instanceRef)]
+  );
+
   const hasPseudoOptions = product.tags.includes('pseudo_options');
 
   return (
     <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12 lg:flex-row lg:gap-8">
-      <div className="h-full w-full basis-full lg:basis-4/6">
-        <Gallery
+      {/* <Gallery
           images={product.variants.map((variant) => ({
             src: variant.image.url,
             altText: variant.image.altText,
             selectedOptions: variant.selectedOptions
           }))}
-        />
+        /> */}
+      <div className="basis-full lg:max-w-2/3">
+        <div ref={sliderRef} className="keen-slider">
+          <div className="keen-slider__slide bg-blue-400">1</div>
+          <div className="keen-slider__slide bg-orange-400">2</div>
+          <div className="keen-slider__slide bg-purple-400">3</div>
+        </div>
+        <div ref={thumbnailRef} className="keen-slider thumbnail">
+          <div className="keen-slider__slide bg-blue-400">1</div>
+          <div className="keen-slider__slide bg-orange-400">2</div>
+          <div className="keen-slider__slide bg-purple-400">3</div>
+        </div>
       </div>
 
-      <div className="basis-full lg:basis-2/6">
+      <div className="">
         <div className="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
           <h1 className="mb-2 text-5xl font-medium">{product.title}</h1>
           <div className="mr-auto w-auto rounded-full bg-blue-600 p-2 text-sm text-white">
