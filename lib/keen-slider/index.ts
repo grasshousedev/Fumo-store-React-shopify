b9634162095f55dd69afcf9770d4db30c0ee587f
Oleksandr F <boltbright525@gmail.com>
2023-10-26 20:42:29 +0400

    create module with utility functions of/for keen-slider

---
+++ lib/keen-slider/index.ts
@@ -0,0 +1,39 @@
+import { KeenSliderInstance, KeenSliderPlugin } from 'keen-slider';
+import { MutableRefObject } from 'react';
+
+export function ThumbnailPlugin(
+  mainRef: MutableRefObject<KeenSliderInstance | null>
+): KeenSliderPlugin {
+  return (slider) => {
+    function removeActive() {
+      slider.slides.forEach((slide) => {
+        slide.classList.remove('active');
+      });
+    }
+    function addActive(idx: number) {
+      // TODO: find out how to correctly handle the 'Object is possibly undefined' problem
+      //! the non-null assertion is a temporary solution
+      slider.slides[idx]!.classList.add('active');
+    }
+
+    function addClickEvents() {
+      slider.slides.forEach((slide, idx) => {
+        slide.addEventListener('click', () => {
+          if (mainRef.current) mainRef.current.moveToIdx(idx);
+        });
+      });
+    }
+
+    slider.on('created', () => {
+      if (!mainRef.current) return;
+      addActive(slider.track.details.rel);
+      addClickEvents();
+      mainRef.current.on('animationStarted', (main) => {
+        removeActive();
+        const next = main.animator.targetIdx || 0;
+        addActive(main.track.absToRel(next));
+        slider.moveToIdx(Math.min(slider.track.details.maxIdx, next));
+      });
+    });
+  };
+}
