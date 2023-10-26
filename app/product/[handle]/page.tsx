485edc5df9c193fb7cb368c62fc1d5648ba15833
Oleksandr F <boltbright525@gmail.com>
2023-10-26 18:27:50 +0400

    move <Gallery> component inside <ProductDescription> component

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -4,7 +4,6 @@ import { Suspense } from 'react';
 
 import { GridTileImage } from 'components/grid/tile';
 import Footer from 'components/layout/footer';
-import { Gallery } from 'components/product/gallery';
 import { ProductDescription } from 'components/product/product-description';
 import { HIDDEN_PRODUCT_TAG } from 'lib/constants';
 import { getProduct, getProductRecommendations } from 'lib/shopify';
@@ -98,21 +97,7 @@ export default async function ProductPage({
         }}
       />
       <div className="mx-auto max-w-screen-2xl px-4">
-        <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12 lg:flex-row lg:gap-8">
-          <div className="h-full w-full basis-full lg:basis-4/6">
-            <Gallery
-              images={product.variants.map((variant) => ({
-                src: variant.image.url,
-                altText: variant.image.altText,
-                selectedOptions: variant.selectedOptions
-              }))}
-            />
-          </div>
-
-          <div className="basis-full lg:basis-2/6">
-            <ProductDescription product={product} selectedVariantPrice={selectedVariant.price} />
-          </div>
-        </div>
+        <ProductDescription product={product} selectedVariantPrice={selectedVariant.price} />
         <Suspense>
           <RelatedProducts id={product.id} />
         </Suspense>
--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -1,4 +1,5 @@
 import { AddToCart } from '@/components/product/add-to-cart';
+import { Gallery } from '@/components/product/gallery';
 import Price from '@/components/ui/price';
 import Prose from '@/components/ui/prose';
 import { Money, Product } from 'lib/shopify/types';
@@ -15,30 +16,45 @@ export function ProductDescription({
   const hasPseudoOptions = product.tags.includes('pseudo_options');
 
   return (
-    <>
-      <div className="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
-        <h1 className="mb-2 text-5xl font-medium">{product.title}</h1>
-        <div className="mr-auto w-auto rounded-full bg-blue-600 p-2 text-sm text-white">
-          <Price
-            amount={selectedVariantPrice.amount}
-            currencyCode={selectedVariantPrice.currencyCode}
-          />
-        </div>
-      </div>
-      {!hasPseudoOptions ? (
-        <VariantSelector options={product.options} variants={product.variants} />
-      ) : (
-        <VariantSelectorWithPseudoOptions option={product.options[0]} variants={product.variants} />
-      )}
-
-      {product.descriptionHtml ? (
-        <Prose
-          className="mb-6 text-sm leading-tight dark:text-white/[60%]"
-          html={product.descriptionHtml}
+    <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12 lg:flex-row lg:gap-8">
+      <div className="h-full w-full basis-full lg:basis-4/6">
+        <Gallery
+          images={product.variants.map((variant) => ({
+            src: variant.image.url,
+            altText: variant.image.altText,
+            selectedOptions: variant.selectedOptions
+          }))}
         />
-      ) : null}
+      </div>
 
-      <AddToCart variants={product.variants} availableForSale={product.availableForSale} />
-    </>
+      <div className="basis-full lg:basis-2/6">
+        <div className="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
+          <h1 className="mb-2 text-5xl font-medium">{product.title}</h1>
+          <div className="mr-auto w-auto rounded-full bg-blue-600 p-2 text-sm text-white">
+            <Price
+              amount={selectedVariantPrice.amount}
+              currencyCode={selectedVariantPrice.currencyCode}
+            />
+          </div>
+        </div>
+        {!hasPseudoOptions ? (
+          <VariantSelector options={product.options} variants={product.variants} />
+        ) : (
+          <VariantSelectorWithPseudoOptions
+            option={product.options[0]}
+            variants={product.variants}
+          />
+        )}
+
+        {product.descriptionHtml ? (
+          <Prose
+            className="mb-6 text-sm leading-tight dark:text-white/[60%]"
+            html={product.descriptionHtml}
+          />
+        ) : null}
+
+        <AddToCart variants={product.variants} availableForSale={product.availableForSale} />
+      </div>
+    </div>
   );
 }
