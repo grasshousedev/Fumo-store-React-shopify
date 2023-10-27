41d7ba25b48857fdadbada7e47b3484aa0f4bf6c
Oleksandr F <boltbright525@gmail.com>
2023-10-27 18:03:00 +0400

    move code of slider's controls into a separate component and import it

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -10,8 +10,7 @@ import { Money, Product } from '@/lib/shopify/types';
 import { AddToCart } from '@/components/product/add-to-cart';
 import { VariantSelector } from '@/components/product/variant-selector';
 import { VariantSelectorWithPseudoOptions } from '@/components/product/variant-selector-pseudo-options';
-import { Button } from '@/components/ui/button';
-import Chevron from '@/components/ui/chevron';
+import SliderControls from '@/components/slider-controls';
 import Price from '@/components/ui/price';
 import Prose from '@/components/ui/prose';
 
@@ -72,28 +71,7 @@ export function ProductDescription({
             ))}
           </div>
           {loaded && instanceRef.current && (
-            <>
-              <Button
-                className="absolute left-4 top-1/2 h-12 -translate-y-1/2"
-                disabled={currentSlide === 0}
-                variant={currentSlide === 0 ? 'ghost' : 'outline'}
-                onClick={() => instanceRef.current?.prev()}
-              >
-                <Chevron width={30} height={30} direction="left" />
-              </Button>
-              <Button
-                className="absolute right-4 top-1/2 h-12 -translate-y-1/2"
-                disabled={currentSlide === instanceRef.current.track.details.slides.length - 1}
-                variant={
-                  currentSlide === instanceRef.current.track.details.slides.length - 1
-                    ? 'ghost'
-                    : 'outline'
-                }
-                onClick={() => instanceRef.current?.next()}
-              >
-                <Chevron width={30} height={30} direction="right" />
-              </Button>
-            </>
+            <SliderControls instanceRefCurrent={instanceRef.current} currentSlide={currentSlide} />
           )}
         </div>
         <div className="w-1/2 basis-1/5">
