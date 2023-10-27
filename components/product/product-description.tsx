33222818f9c872cc251b71d6580eeda039509836
Oleksandr F <boltbright525@gmail.com>
2023-10-27 17:23:01 +0400

    add navigation arrows to the slider

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -1,17 +1,19 @@
 'use client';
 
+import { useKeenSlider } from 'keen-slider/react';
 import Image from 'next/image';
-
-import { AddToCart } from '@/components/product/add-to-cart';
-// import { Gallery } from '@/components/product/gallery';
-import Price from '@/components/ui/price';
-import Prose from '@/components/ui/prose';
-import { Money, Product } from 'lib/shopify/types';
-import { VariantSelector } from './variant-selector';
-import { VariantSelectorWithPseudoOptions } from './variant-selector-pseudo-options';
+import { useState } from 'react';
 
 import { ThumbnailPlugin } from '@/lib/keen-slider';
-import { useKeenSlider } from 'keen-slider/react';
+import { Money, Product } from '@/lib/shopify/types';
+
+import { AddToCart } from '@/components/product/add-to-cart';
+import { VariantSelector } from '@/components/product/variant-selector';
+import { VariantSelectorWithPseudoOptions } from '@/components/product/variant-selector-pseudo-options';
+import { Button } from '@/components/ui/button';
+import Chevron from '@/components/ui/chevron';
+import Price from '@/components/ui/price';
+import Prose from '@/components/ui/prose';
 
 export function ProductDescription({
   product,
@@ -20,10 +22,15 @@ export function ProductDescription({
   product: Product;
   selectedVariantPrice: Money;
 }) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
     created() {
-      console.log('slider created');
+      setLoaded(true);
     }
   });
 
@@ -48,41 +55,52 @@ export function ProductDescription({
 
   return (
     <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12 lg:flex-row lg:gap-8">
-      {/* <Gallery
-          images={product.variants.map((variant) => ({
-            src: variant.image.url,
-            altText: variant.image.altText,
-            selectedOptions: variant.selectedOptions
-          }))}
-        /> */}
       <div className="flex basis-full flex-col items-center gap-6 lg:max-w-2/3">
-        <div ref={sliderRef} className="keen-slider basis-4/5">
-          {images.map((image) => (
-            <div key={image.src} className="keen-slider__slide relative h-full w-full">
-              <Image
-                src={image.src}
-                alt={image.altText || ''}
-                className="object-contain"
-                sizes="(min-width: 1024px) 66vw, 100vw"
-                fill
-                priority
-              />
-            </div>
-          ))}
+        <div className="relative w-full basis-4/5">
+          <div ref={sliderRef} className="keen-slider h-full">
+            {images.map((image) => (
+              <div key={image.src} className="keen-slider__slide relative h-full w-full">
+                <Image
+                  src={image.src}
+                  alt={image.altText || ''}
+                  className="object-contain"
+                  sizes="(min-width: 1024px) 66vw, 100vw"
+                  fill
+                  priority
+                />
+              </div>
+            ))}
+          </div>
+          {loaded && instanceRef.current && (
+            <>
+              <Button
+                className="absolute left-4 top-1/2 h-12 -translate-y-1/2"
+                disabled={currentSlide === 0}
+                variant={currentSlide === 0 ? 'ghost' : 'outline'}
+                onClick={() => instanceRef.current?.prev()}
+              >
+                <Chevron width={30} height={30} direction="left" />
+              </Button>
+              <Button
+                className="absolute right-4 top-1/2 h-12 -translate-y-1/2"
+                disabled={currentSlide === instanceRef.current.track.details.slides.length - 1}
+                variant={
+                  currentSlide === instanceRef.current.track.details.slides.length - 1
+                    ? 'ghost'
+                    : 'outline'
+                }
+                onClick={() => instanceRef.current?.next()}
+              >
+                <Chevron width={30} height={30} direction="right" />
+              </Button>
+            </>
+          )}
         </div>
         <div className="w-1/2 basis-1/5">
           <div ref={thumbnailRef} className="keen-slider thumbnail h-full">
             {images.map((image) => (
-              <div className="keen-slider__slide relative">
-                <Image
-                  className="object-cover"
-                  key={image.src}
-                  src={image.src}
-                  alt={image.altText || ''}
-                  // width={100}
-                  // height={100}
-                  fill
-                />
+              <div key={image.src} className="keen-slider__slide relative">
+                <Image className="object-cover" src={image.src} alt={image.altText || ''} fill />
               </div>
             ))}
           </div>
