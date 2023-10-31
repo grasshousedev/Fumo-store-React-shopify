c8a2c2833fe4e6c0d41efa7424e3c40164d7df1f
Oleksandr F <boltbright525@gmail.com>
2023-10-31 18:22:41 +0400

    remove unwanted text

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -99,7 +99,7 @@ export function ProductDescription({
         </div>
       </div>
 
-      <div className="">
+      <div>
         <div className="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
           <h1 className="mb-2 text-5xl font-medium">{product.title}</h1>
           <div className="mr-auto w-auto rounded-full bg-blue-600 p-2 text-sm text-white">
@@ -118,7 +118,6 @@ export function ProductDescription({
             variants={product.variants}
           />
         )}
-        track.details.slides.length
         {product.descriptionHtml ? (
           <Prose
             className="mb-6 text-sm leading-tight dark:text-white/[60%]"
