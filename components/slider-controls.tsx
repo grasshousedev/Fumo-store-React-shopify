8098a68dc17c69250d9909b53586ab82574fbf5f
Oleksandr F <boltbright525@gmail.com>
2023-10-31 11:58:22 +0400

    use 'details.maxIdx' instead 'details.slides.length' to check if slide is the last one

--- components/slider-controls.tsx
+++ components/slider-controls.tsx
@@ -22,10 +22,8 @@ export default function SliderControls({
       </Button>
       <Button
         className="absolute right-4 top-1/2 h-12 -translate-y-1/2"
-        disabled={currentSlide === instanceRefCurrent.track.details.slides.length - 1}
-        variant={
-          currentSlide === instanceRefCurrent.track.details.slides.length - 1 ? 'ghost' : 'outline'
-        }
+        disabled={currentSlide === instanceRefCurrent.track.details.maxIdx}
+        variant={currentSlide === instanceRefCurrent.track.details.maxIdx ? 'ghost' : 'outline'}
         onClick={() => instanceRefCurrent.next()}
       >
         <Chevron width={30} height={30} direction="right" />
