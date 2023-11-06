b4dbdae2ba81730dfaa531545e62610a6728bff4
Oleksandr F <boltbright525@gmail.com>
2023-11-06 21:30:15 +0400

    remove logo from home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import { Suspense } from 'react';
 
 import Footer from 'components/layout/footer';
-import Logo from 'components/logo';
 
 import { buttonVariants } from '@/components/ui/button';
 import { ChevronRightIcon } from '@heroicons/react/24/outline';
@@ -23,7 +22,6 @@ export default async function HomePage() {
     <>
       <Suspense>
         <div className="mx-auto max-w-screen-xl px-4 py-4 text-center">
-          <Logo className="mx-auto mb-8 w-2/3 max-w-md" /> {/* //TODO change mb */}
           <CollectionsList />
           <Link href="/search" className={buttonVariants({ size: 'lg', className: 'gap-1' })}>
             <span className="text-base">See all our collections</span>
