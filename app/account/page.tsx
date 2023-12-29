4dc0812cfe142aa759f9c50f0f0624c5bd8c8350
Oleksandr F <boltbright525@gmail.com>
2023-12-29 17:48:32 +0400

    setup code to make requests and display info from Customer Account API

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,42 @@
 import { cookies } from 'next/headers';
 
-export default function Account() {
+export default async function Account() {
   const accessToken = cookies().get('access_token');
 
-  return accessToken ? <p>Account info</p> : <p>You must log in</p>;
+  if (!accessToken) return <p>You must log in</p>;
+
+  const getUserNameQuery = `
+    query getUserName {
+      customer {
+        firstName
+      }
+    }
+  `;
+
+  const getUserNameRes = await fetch(
+    `https://shopify.com/${process.env.SHOP_ID}/account/customer/api/unstable/graphql`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: accessToken.value
+      },
+      body: JSON.stringify({
+        operationName: 'getUserName',
+        query: getUserNameQuery,
+        variables: {}
+      })
+    }
+  );
+
+  const data = await getUserNameRes.json();
+
+  console.log(data.data.customer.firstName);
+
+  return (
+    <div>
+      <h1>Account info</h1>
+      <p>{data.data.customer.firstName}</p>
+    </div>
+  );
 }
