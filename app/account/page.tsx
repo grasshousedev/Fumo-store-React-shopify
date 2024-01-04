3d0e64da2b26f52aa58226fc0e9bc3dba7b0e648
Oleksandr F <boltbright525@gmail.com>
2024-01-04 18:06:07 +0400

    render customer's orders

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,8 @@
+import Price from '@/components/ui/price';
 import { getCustomer } from '@/lib/shopify';
+import clsx from 'clsx';
 import { cookies } from 'next/headers';
+import Image from 'next/image';
 
 export default async function Account() {
   const accessToken = cookies().get('access_token');
@@ -21,16 +24,71 @@ export default async function Account() {
         <p className="col-start-2 row-start-1">E-mail: {customer.emailAddress?.emailAddress}</p>
         <p>Phone number: {customer.phoneNumber?.phoneNumber}</p>
       </div>
-      <p>Orders</p>
-      <ul>
-        {customer.orders.map(function (order) {
-          return (
-            <li key={order.name}>
-              <p>Order: {order.name}</p>
-              <p>Total price: {order.totalPrice.amount}</p>
-            </li>
-          );
-        })}
+      <h2 className="text-center text-2xl">Orders</h2>
+      <ul className="rounded-sm border border-neutral-200 px-3 py-4 dark:border-neutral-800">
+        {customer.orders.map((order, i, arr) => (
+          <li
+            key={order.name}
+            className={clsx(
+              'rounded-sm border border-neutral-200 px-3 py-4 dark:border-neutral-800',
+              {
+                'mb-6': i + 1 < arr.length // don't add a margin to the last element of the list
+              }
+            )}
+          >
+            <div className="mb-3 flex justify-between">
+              <p>
+                Order {order.name} dated {order.processedAt}
+              </p>
+              <div>
+                <span>Total: </span>
+                <Price
+                  className="inline-block text-right text-base font-semibold text-black dark:text-white"
+                  amount={order.totalPrice.amount}
+                  currencyCode={order.totalPrice.currencyCode}
+                />
+              </div>
+            </div>
+            <ul>
+              {order.lineItems.map((item, i, arr) => (
+                <li
+                  className={clsx(
+                    'flex gap-6 rounded-sm border border-neutral-200 bg-neutral-100 px-3 py-4 dark:border-neutral-800 dark:bg-neutral-900',
+                    {
+                      'mb-4': i + 1 < arr.length // don't add a margin to the last element of the list
+                    }
+                  )}
+                >
+                  <div className="flex flex-col justify-between">
+                    <p>{item.name}</p>
+                    <Price
+                      className="w-fit rounded-full bg-blue-600 p-2 text-white"
+                      amount={item.price.amount}
+                      currencyCode={item.price.currencyCode}
+                    />
+                  </div>
+                  <div className="ml-auto flex flex-col justify-between text-right">
+                    <div>
+                      <Price
+                        className="inline text-right text-base font-medium text-black dark:text-white"
+                        amount={item.totalPrice.amount}
+                        currencyCode={item.totalPrice.currencyCode}
+                      />
+                    </div>
+                    <p className="text-sm">Quantity: {item.quantity}</p>
+                  </div>
+                  <Image
+                    className="order-first h-20 w-20 rounded-sm object-cover"
+                    src={item.image.url}
+                    alt={item.image.altText || ''}
+                    width={80}
+                    height={80}
+                  />
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
       </ul>
     </div>
   );
