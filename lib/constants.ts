6f87949c2a031f39d6c86c609cb04874fc1d41f6
Oleksandr F <boltbright525@gmail.com>
2024-01-19 21:45:48 +0400

    update Shopify Storefront API endpoint from 2023-01 to 2024-01 version

--- lib/constants.ts
+++ lib/constants.ts
@@ -30,5 +30,5 @@ export const TAGS = {
 
 export const HIDDEN_PRODUCT_TAG = 'nextjs-frontend-hidden';
 export const DEFAULT_OPTION = 'Default Title';
-export const SHOPIFY_STOREFRONT_API_ENDPOINT = '/api/2023-01/graphql.json';
+export const SHOPIFY_STOREFRONT_API_ENDPOINT = '/api/2024-01/graphql.json';
 export const SHOPIFY_CUSTOMER_ACCOUNT_API_ENDPOINT = `/${process.env.SHOP_ID}/account/customer/api/unstable/graphql`;
